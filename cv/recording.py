dc89d976979d07a175cdeefdde9dc035bfc6f7f4
Daniel Lew <lewdaniel2@gmail.com>
2025-06-07 10:27:52 -0400

    Finished button stop and start

--- cv/recording.py
+++ cv/recording.py
@@ -1,13 +1,17 @@
 import os
 import cv2 as cv
 from datetime import datetime
-from time import time
+from time import time, sleep
+from gpiozero import Button
 
 class Recording:
     def __init__(self):
         self.camera = cv.VideoCapture(0)
+        self.button = Button(17)
+        self.button_fg = 0
+        self.ret_val = 1
 
-    def start_recording(self, cam_index, save_folder = ".", vid_len = 60, fps = 30):
+    def start_recording(self, save_folder = ".", vid_len = 60, fps = 30):
         cam = self.camera
         if cam.isOpened():
             frame_width = int(cam.get(cv.CAP_PROP_FRAME_WIDTH))
@@ -21,14 +25,19 @@ class Recording:
 
         start_time = time()
         curr_time = start_time
-        ret_val = 1
+        self.ret_val = 1
         while cam.isOpened() and curr_time-start_time < vid_len:
+            if self.button.is_pressed:
+                while self.button.is_pressed:
+                    sleep(.001)
+                self.button_fg = 1
             ret, frame = cam.read()
             if ret:
                 out.write(frame)
                 # cv.imshow('Frame', frame)
-                if cv.waitKey(1) & 0xFF == ord('q'):
-                    ret_val = 0
+                if self.button_fg == 1:
+                    self.button_fg = 0
+                    self.ret_val = 0
                     break
             else:
                 raise Exception("Something went wrong with cam")
@@ -37,22 +46,27 @@ class Recording:
         cv.destroyAllWindows()
 
         print(f"{time_now}.mp4 saved!")
-        return ret_val
 
-    def start_refresh_recording(self, cam_index, vid_len = 10, fps = 30):
+    def start_refresh_recording(self, vid_len = 10, fps = 30):
         cam = self.camera
-        time_now = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-        os.makedirs(time_now)
-        while cam.isOpened():
-            ret = self.start_recording(0, time_now, vid_len)
-            if not ret:
-                break
+        while 1:
+            time_now = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+            os.makedirs(time_now)
+            self.button.wait_for_press()
+            while self.button.is_pressed:
+                sleep(.001)
+            print("recording started")
+            while cam.isOpened():
+                self.start_recording(time_now, vid_len)
+                if not self.ret_val:
+                    print("recording stopped")
+                    break
         cam.release()
     
 
     def stop_recording(self):
-        return None
+        return 0
 
 if __name__ == "__main__":
     camera = Recording()
-    camera.start_refresh_recording(0)
+    camera.start_refresh_recording()
