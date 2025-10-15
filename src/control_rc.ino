af29f7c404d9f21c5f515256f23129956bda277d
Daniel Lew <lewdaniel2@gmail.com>
2025-10-15 19:34:48 -0400

    updated to accept range - to 1

--- src/control_rc.ino
+++ src/control_rc.ino
@@ -6,39 +6,55 @@ Servo esc;
 
 void setup() {
   Serial.begin(9600);
-  // Attach the servo to pin 3
-  //myServo.attach(3);
-  delay(10000);
+  
+  // Wait for serial to be available
+  while(!Serial.available()) { delay(2000); }
+  // Block until read signal is sent
+  static char buf[96];
+  while(Serial.available()) {
+    int len = Serial.readBytesUntil('\n', buf, sizeof(buf)-1);
+    buf[len] = '\0';
+    char ready_check[100];
+    if(sscanf(buf, "%s", &ready_check) == 1) {
+        if(strcmp(ready_check, "PI-READY") == 0) {
+          Serial.println("ACK");
+          break;
+        }
+    }
+    else { Serial.println("Received Incorrect Signal"); }
+  }
+  // Attach the esc to pin 9
   esc.attach(9,1000,2000);
-  // Initialize at 90 degs
-  //myServo.write(2000);
+  // Attach the servo to pin 3
+  servo.attach(3);
+
   esc.writeMicroseconds(2000);
   delay(3000);
   esc.writeMicroseconds(1000);
   delay(3000);
-  //esc.writeMicroseconds(1500);
+
+  // Initialize at 90 degs
+  servo.write(90);
 }
 
 void loop() {
   static char buf[96];
-  esc.writeMicroseconds(1200);
-  delay(3000);
-  esc.writeMicroseconds(1000);
-  delay(3000);
-  ;
-  /*
   while(Serial.available()) {
     int len = Serial.readBytesUntil('\n', buf, sizeof(buf)-1);
     if (len <= 0) return;
     buf[len] = '\0';
 
-    int ch1, ch3, ch4;
-    if(sscanf(buf, "%d %d %d", &ch1, &ch3, &ch4) == 3) {
-      //myServo.write(ch1);
-      esc.writeMicroseconds(ch1);
+    // channels should range from -1 to 1
+    float ch1, ch3;
+    if(sscanf(buf, "%f", &ch1, &ch3) == 2) {
+      // Convert -1 to 1 to 0 to 180
+      int rudder = ((ch1+1)/2)*180
+      // Convert -1 to 1 to 1000
+      int throttle = ((ch3+2)*1000
+      servo.write(rudder);
+      esc.writeMicroseconds(throttle);
     } else {
       Serial.print("bad value");
     }
   }
-  */
 }
