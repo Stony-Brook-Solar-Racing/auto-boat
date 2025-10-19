f82eed28b6ba0558ca844eeeb7c07e5220dd4dcf
sadneo <haywardeel@gmail.com>
2025-10-19 14:54:34 -0400

    refactor: optimize recursion

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
-from glob import glob
-from serial import Serial, SerialException
-from time import sleep
 import sys
+from glob import glob
+from time import sleep
+
+from serial import Serial, SerialException
 
 from decode_rc import Decode
 
 # Constants
 BAUDRATE = 9600
 TIMEOUT = 1
+
 
 def _send(arduino, text):
     line = text.encode("ascii")
@@ -18,36 +20,38 @@ def _send(arduino, text):
     arduino.write(line)
     arduino.flush()
 
+
 def _setup_arduino():
     arduino = None
     # Check platform type
     if sys.platform == "linux":
         ports = glob("/dev/ttyACM*")
-    elif sys.platform.startswith('win'):
+    elif sys.platform.startswith("win"):
         ports = [f"COM{i}" for i in range(10)]
     else:
-       raise Exception("Platform not supported")
+        raise Exception("Platform not supported")
 
     # Find arduino
-    for port in ports:
-        try:
-            arduino = Serial(port=port, baudrate=BAUDRATE, timeout=TIMEOUT)
-            sleep(1)
-        except(OSError, SerialException) as err:
-            # logging.error(err)
-            print(err)
-    
-    # Keep retrying if arduino is not found
     while arduino == None:
+        for port in ports:
+            try:
+                arduino = Serial(port=port, baudrate=BAUDRATE, timeout=TIMEOUT)
+                sleep(1)
+            except (OSError, SerialException) as err:
+                # logging.error(err)
+                print(err)
+
+        # Send ready signal to arduino if good
+        if arduino != None:
+            ready = "PI-READY\n"
+            _send(arduino, ready)
+            return arduino
+
+        # Keep retrying if arduino is not found
         print("Arduino not found\n")
         print("Retrying in 5 seconds...")
         sleep(5)
-        _setup_arduino()
 
-    # Send ready signal to arduino
-    ready = "PI-READY\n" 
-    _send(arduino, ready)
-    return arduino
 
 if __name__ == "__main__":
     arduino = _setup_arduino()
@@ -56,7 +60,7 @@ if __name__ == "__main__":
     while True:
         (sa, ch1, ch3) = rc_decoder.decode_rc()
         if sa == "1":
-            channels = ch1+" "+ch3+"\n"
+            channels = ch1 + " " + ch3 + "\n"
             print(channels)
             _send(arduino, channels)
         elif sa == "0":
