5f1e39122647b71f5e664d8450d2cab133742988
QuiteKewl <iamxuhanqiu@gmail.com>
2025-12-03 15:51:37 -0500

    feat: auto fix

---
+++ src/auto.py
@@ -0,0 +1,42 @@
+import math
+
+class Auto:
+
+    def __init__ (self, gps, PORT_COMPASS):
+        self.gps = gps
+        self.PORT_COMPASS = PORT_COMPASS
+        
+    def distance (point1, point2): # Should be in degress
+        D_lon = (point2.longtitude - point1.longtitude) * math.pi / 180.0
+        D_lat = (point2.latitude - point1.latitude) * math.pi / 180.0
+        
+        lat1 = (point1.latitude) * math.pi / 180.0
+        lat2 = (point2.latitude) * math.pi / 180.0
+
+        a = (pow(math.sin(D_lat / 2), 2) + 
+             pow(math.sin(D_lon / 2), 2) * 
+                 math.cos(lat1) * math.cos(lat2));
+        print(point1.latitude, point2.latitude, lat1, lat2, a)
+        rad = 6371
+        c = 2 * math.asin(math.sqrt(a))
+        return rad * c
+
+    def speed (dist, time): #Use dist func above, time is set
+        return dist/time
+
+class Point:
+    def __init__ (self, latitude, longtitude):
+        self.longtitude = longtitude
+        self.latitude = latitude
+
+print("lon -180 to 180 and lat -90 to 90\n ")
+point1 = Point(11.0, 22.0)
+point2 = Point(33.0, 44.0)
+print(Auto.distance(point1, point2))
+        
+    
+    
+
+
+
+
