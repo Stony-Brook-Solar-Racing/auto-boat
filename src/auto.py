d3100b69ec49660ef0395ae2741a4727150980e8
AntonYanaky <antonyanaky@gmail.com>
2025-12-08 18:11:42 -0500

    added bearing calculation

--- src/auto.py
+++ src/auto.py
@@ -1,8 +1,8 @@
 import math
-from gps import Gps, Point
+from gps import Gps, Point, Compass
 
 class Auto:
-    def __init__(self, gps, compass):
+    def __init__(self, gps: Gps, compass: Compass):
         self.gps = gps
         self.compass = compass
         
@@ -23,3 +23,19 @@ class Auto:
 
     def speed(self, dist, time): #Use dist func above, time is set
         return dist/time
+    
+    def a_to_w(self, point1: Point, point2: Point) -> float:
+        dLon = (point2.longitude - point1.longitude) * math.pi / 180.0
+        lat1 = (point1.latitude) * math.pi / 180.0
+        lat2 = (point2.latitude) * math.pi / 180.0
+
+        y = math.sin(dLon) * math.cos(lat2)
+        x = (math.cos(lat1) * math.sin(lat2) - 
+             math.sin(lat1) * math.cos(lat2) * math.cos(dLon))
+        brng = math.atan2(y, x)
+        brng = math.degrees(brng)
+        brng = (brng + 360) % 360
+        return brng - self.compass.get_heading()
+    
+
+
