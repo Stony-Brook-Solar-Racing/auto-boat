d89e93c932e161c5db38b6428ddc337573690a73
Daniel Lew <lewdaniel2@gmail.com>
2025-12-08 17:13:23 -0500

    feat: compass heading

--- src/navigation.py
+++ src/navigation.py
@@ -38,13 +38,13 @@ class Gps:
 
 class Compass:
     def __init__(self, bus_num=1, addr=0x1E, declination_deg=0.0):
-        self.bus = smbus.SMBus(bus_num)
+        self.compass = smbus.SMBus(bus_num)
         self.addr = addr
         self.declination_rad = math.radians(declination_deg)
 
-        self.bus.write_byte_data(self.addr, 0x00, 0x70)
-        self.bus.write_byte_data(self.addr, 0x01, 0x20)
-        self.bus.write_byte_data(self.addr, 0x02, 0x00)
+        self.compass.write_byte_data(self.addr, 0x00, 0x70)
+        self.compass.write_byte_data(self.addr, 0x01, 0x20)
+        self.compass.write_byte_data(self.addr, 0x02, 0x00)
         time.sleep(1)
 
     @staticmethod
@@ -53,8 +53,21 @@ class Compass:
             val -= (1 << bits)
         return val
 
-    def _read_axis():
+    def _read_axis(self):
+        data = self.compass.read_i2c_block_data(self.addr, 0x03, 6)
+        x = self._twos_complement(data[0] << 8 | data[1], 16)
+        z = self._twos_complement(data[2] << 8 | data[3], 16)
+        y = self._twos_complement(data[4] << 8 | data[5], 16)
+        return x, y, z
         
 
     def get_heading(self):
-        pass
+        x, y, _ = self._read_axis()
+        # In radians btw
+        heading = math.atan2(y,x)
+        if heading < 0:
+            heading += 2 * math.pi
+        if heading > 2 * math.pi:
+            heading -= 2 * math.pi
+
+        return math.degrees(heading)
