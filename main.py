c6ce90687dfb9611f717ebb24ee5e82e00f5be5e
Daniel Lew <lewdaniel2@gmail.com>
2026-03-09 13:20:17 -0400

    fix: distance formula

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from auto-boat!")
-
-
-if __name__ == "__main__":
-    main()
--- src/auto.py
+++ src/auto.py
@@ -7,12 +7,11 @@ def distance(point1: Point, point2: Point) -> float: # Returns in km
     D_lon = (point2.longitude - point1.longitude) * math.pi / 180.0
     
     lat1 = (point1.latitude) * math.pi / 180.0
-    lat2 = (point2.longitude) * math.pi / 180.0
+    lat2 = (point2.latitude) * math.pi / 180.0
 
     a = (pow(math.sin(D_lat / 2), 2) + 
          pow(math.sin(D_lon / 2), 2) * 
              math.cos(lat1) * math.cos(lat2));
-    # print(point1.latitude, point2.latitude, lat1, lat2, a)
     rad = 6371
     c = 2 * math.asin(math.sqrt(a))
     return rad * c
